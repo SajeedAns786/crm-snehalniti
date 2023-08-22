3efd01feb5cbc30ab7691bad95690bb6c917cd82
Sajeed Ansari <sajeedans.333@gmail.com>
2023-08-22 18:20:17 +0530

    d1

--- dashboard.php
+++ dashboard.php
@@ -9,7 +9,27 @@ if (!isset($_SESSION['user_id'])) {
 
 // Check the user's role
 if ($_SESSION['user_role'] === 'admin') {
-    echo "hello admin";
+    $dashboardContent = 
+    "<html>
+    <head>
+        <title>Admin Dashboard</title>
+    </head>
+    <body>
+        <h1>Welcome to the Admin Dashboard</h1>
+        <p>Here, you can manage administrative tasks.</p>
+        <h2>List of Leads</h2>
+        <ul>
+            <li>Lead 1: John Doe</li>
+            <li>Lead 2: Jane Smith</li>
+            <li>Lead 3: Bob Johnson</li>
+            <!-- Add more leads as needed -->
+        </ul>
+        <p><a href='admin_tasks.php'>Go to Admin Tasks</a></p>
+    </body>
+    </html>";
+
+    echo $dashboardContent;
+    
 } elseif ($_SESSION['user_role'] === 'teamleader') {
     echo "hello teamleader";
 }elseif ($_SESSION['user_role'] === 'telecaller') {
